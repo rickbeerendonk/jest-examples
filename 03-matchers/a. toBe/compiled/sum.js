32cea0d079d570a6827966c907b5fe664c620f41
Rick Beerendonk <rick@beerendonk.com>
2020-01-09 08:12:51 +0100

    Simplified and extra toBe matcher test.

--- 03-matchers/a. toBe/compiled/sum.js
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-function sum(a, b) {
-  return a + b;
-}
-
-module.exports = sum;
--- 03-matchers/a. toBe/compiled/sum.test.js
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-const sum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
