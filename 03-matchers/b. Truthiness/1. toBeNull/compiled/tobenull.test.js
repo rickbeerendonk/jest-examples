228da778a254c2e9c23bf3ccfd4282318a9630c2
Rick Beerendonk <rick@beerendonk.com>
2020-04-26 09:28:36 +0200

    Add "matchers - Truthiness - toBeNull" examples.

---
+++ 03-matchers/b. Truthiness/1. toBeNull/compiled/tobenull.test.js
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+test('null must be null', () => {
+  expect(null).toBeNull();
+});
+
+test('undefined must not to be null', () => {
+  expect(undefined).not.toBeNull();
+});
+
+test('false must not to be null', () => {
+  expect(false).not.toBeNull();
+});
+
+test('Zero must not to be null', () => {
+  expect(0).not.toBeNull();
+});
+
+test('Empty string must not to be null', () => {
+  expect('').not.toBeNull();
+});
