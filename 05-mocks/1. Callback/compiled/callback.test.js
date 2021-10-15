366c0cbe1558607c1e70ab92e6cde310c3e36cdf
Rick Beerendonk <rick@beerendonk.com>
2021-10-15 03:09:40 +0200

    Add "Jest Mocks: Callback" example.

---
+++ 05-mocks/1. Callback/compiled/callback.test.js
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+function foo(initialValue, callbackFn) {
+  return callbackFn(initialValue);
+}
+
+test('Callback is called with correct value and result is used', () => {
+  // Create mock
+  const mockFn = jest.fn((x) => x + 1);
+
+  // Act
+  const result = foo(42, mockFn);
+
+  // Callback should be called once
+  expect(mockFn.mock.calls.length).toBe(1);
+
+  // The first argument of the call to the function was 42
+  expect(mockFn.mock.calls[0][0]).toBe(42);
+
+  // The return value of the first call to the function was 43
+  expect(mockFn.mock.results[0].value).toBe(43);
+
+  // The return is 43
+  expect(result).toBe(43);
+});
